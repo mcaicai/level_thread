aaec5580287424a38f18c9dee747c21d28381631
czgw <1539078422@qq.com>
2022-10-10 21:01:32 +0800

    day9 commit

---
+++ day9/sem_w.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <signal.h>
+void delsemfile()
+{
+	sem_unlink("mysem_w");
+	exit(0);
+}
+
+int main(int arg,char* argv)
+{
+	sem_t *sem_r,*sem_w;
+	key_t key;
+	int shmid;
+	char *shmaddr;
+
+	struct sigaction act;
+	act.sa_handler = delsemfile;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+
+	sigaction(SIGINT,&act,NULL);
+
+
+
+	key = ftok(".",100);
+	if(key<0)
+	{
+		perror("ftok");
+		return 0;	
+	}
+	shmid = shmget(key,500,0666|IPC_CREAT);
+	if(shmid < 0)
+	{
+		perror("shmget");
+		return 0;
+	}
+	shmaddr= shmat(shmid,NULL,0);
+	
+	sem_r= sem_open("mysem_r",O_CREAT|O_RDWR,0666,0);
+	sem_w= sem_open("mysem_w",O_CREAT|O_RDWR,0666,1);
+	
+	
+	while(1)
+	{
+		sem_wait(sem_w);	
+		printf(">");
+		fgets(shmaddr,500,stdin);
+		sem_post(sem_r);
+	}
+
+}
